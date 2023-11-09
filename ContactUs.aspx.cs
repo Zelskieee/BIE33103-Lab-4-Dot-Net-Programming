0ef34197e41cb668f4f373b082ef3494d10758b4
MOHAMAD ARIF AZINUDDIN BIN ZAIDI <116942193+Zelskieee@users.noreply.github.com>
2023-11-09 23:17:07 +0800

    Add files via upload
    Updated ContactUs.aspx.cs, ContactUs.aspx, Login.aspx.cs & Login.aspx

---
+++ ContactUs.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Web.UI;
+using System.Data;
+using System.Data.SqlClient;
+using System.Xml.Linq;
+using System.Drawing;
+using System.Web.UI.WebControls;
+using System.Web.Services.Description;
+
+namespace Lab3.MasterPages
+{
+    public partial class ContactUs : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void submitForm_Click(object sender, EventArgs e)
+        {
+            string ID = id.Text;
+            string Name = name.Text;
+            string Email = email.Text;
+            string Comment = comment.Text;
+
+            SqlConnection con = new SqlConnection
+                ("Data Source = LAPTOP-BM8BRT21\\SQLEXPRESS; Initial Catalog = TestDatabase; Integrated Security = True; Pooling = False");
+
+            SqlCommand com = new SqlCommand();
+
+            try
+            {
+                con.Open();
+
+                com.Connection = con;
+
+                //Response.Write("Successful\n");
+
+                SqlDataAdapter cmd = new SqlDataAdapter();// Create a object of SqlDataAdapter class
+                cmd.InsertCommand = new SqlCommand("INSERT INTO contactus VALUES (@id, @name, @email, @comment) ", con); //Pass the connection object to cmd
+
+                cmd.InsertCommand.Parameters.Add("@id", SqlDbType.VarChar).Value = ID;
+                cmd.InsertCommand.Parameters.Add("@name", SqlDbType.Text).Value = Name;
+                cmd.InsertCommand.Parameters.Add("@email", SqlDbType.Text).Value = Email;
+                cmd.InsertCommand.Parameters.Add("@comment", SqlDbType.Text).Value = Comment;
+                
+                cmd.InsertCommand.ExecuteNonQuery(); //to execute the SQL command
+            }
+            catch (Exception ex)
+            {
+                Response.Write("Error: " + ex.ToString());
+
+            }
+            finally
+            {
+                con.Close();
+
+                id.Text = string.Empty;
+                name.Text = string.Empty;
+                email.Text = string.Empty;
+                comment.Text = string.Empty;
+            }
+        }
+    }
+}
---
+++ Login.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Web.UI;
+
+namespace Lab3.MasterPages
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ValidationSettings.UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
+            lblStatus.Visible = false; // Initially hide the status label.
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string ID = id.Text;
+            string Password = pw.Text;
+
+            string connectionString = "Data Source=LAPTOP-BM8BRT21\\SQLEXPRESS; Initial Catalog=TestDatabase; Integrated Security=True; Pooling=False";
+
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    con.Open();
+
+                    using (SqlCommand cmd = new SqlCommand("SELECT id, pw FROM login WHERE id = @id AND pw = @pw", con))
+                    {
+                        cmd.Parameters.AddWithValue("@id", ID);
+                        cmd.Parameters.AddWithValue("@pw", Password);
+
+                        SqlDataReader reader = cmd.ExecuteReader();
+
+                        if (reader.Read())
+                        {
+                            Response.Redirect("default.aspx");
+                        }
+                        else
+                        {
+                            lblStatus.Visible = true;
+                            lblStatus.Text = "Invalid ID or password";
+                        }
+                        reader.Close();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("An error occurred: " + ex.Message);
+            }
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            // Clear the text inside ID and Password TextBoxes.
+            id.Text = string.Empty;
+            pw.Text = string.Empty;
+            lblStatus.Visible = false; // Hide the status label.
+        }
+    }
+}
